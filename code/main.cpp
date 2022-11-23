33c7868b4786c41cc23d29577675b68677007016
admin <admin@BCS-Ubuntu-28.lrccd.ad.losrios.edu>
2022-11-22 16:01:00 -0800

    Added render window o main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
-using namespace std;
+using namespace sf;
 
 int main()
 {
-    cout << "hello" << endl;
+    VideoMode vm(1920, 1080);
+    RenderWindow window(vm, "Cyber Punk Attack!!!", Style::Default);
+    //cout << "hello" << endl;
+    while (window.isOpen())
+    {
+    /*
+        ****************************************
+        Handle the players input
+        ****************************************
+        */
+        if (Keyboard::isKeyPressed(Keyboard::Escape))
+        {
+            window.close();
+        }
+        /*
+        ****************************************
+        Update the scene
+        ****************************************
+        */
+        /*
+        ****************************************
+        Draw the scene
+        ****************************************
+        */
+        // Clear everything from the last frame
+        window.clear();
+        // Draw our game scene here
+        // Show everything we just drew
+        window.display();
+    }
     return 0;
+
 }
